772ccee1f3f8ddba251f1eb7c970ad0dd829f2dc
Christopher Peplin <peplin@bueda.com>
2011-01-28 17:42:03 -0500

    Add autobench-style repeated httperf runs. Code borrowed from https://github.com/igrigorik/autoperf.

--- httperf.rb
+++ httperf.rb
@@ -30,10 +30,11 @@ require 'optparse'
 require 'ostruct'
 require 'yaml'
 require 'base64'
+require 'ruport'
 
-VALID_OPTIONS = ['server', 'rate', 'port', 'connections', 'send_buffer',
-  'recv_buffer', 'uri_list', 'httperf', 'host', 'username', 'password',
-  'num_call', 'hog']
+VALID_OPTIONS = ['server', 'rate', 'low_rate', 'high_rate', 'rate_step',
+  'wait_time', 'port', 'connections', 'send_buffer', 'recv_buffer', 'uri_list',
+  'httperf', 'host', 'username', 'password', 'num_call', 'hog']
 
 class OpenStruct
   def add_new(name, value)
@@ -64,8 +65,25 @@ class HttperfRunner
       # Check to make sure we have a valid httperf
       check_for_httperf
 
-      # Run the script
-      run_httperf
+      results = {}
+      report = Table(:column_names => ['rate', 'conn/s', 'req/s',
+                     'replies/s avg', 'errors', '5xx status', 'net io (KB/s)'])
+
+      if not @options.low_rate or not @options.high_rate
+        @options.low_rate = @options.high_rate = @options.rate
+      end
+
+      if @options.low_rate and @options.high_rate
+        (@options.low_rate..@options.high_rate).step(@options.rate_step) do |rate|
+          results[rate] = run_httperf rate
+          report << results[rate].merge({'rate' => rate})
+
+          puts report.to_s
+          puts results[rate]['output'] if (results[rate]['errors'].to_i > 0 or
+              results[rate]['5xx status'].to_i > 0)
+        end
+      else
+      end
     else
       output_usage
     end
@@ -82,19 +100,42 @@ class HttperfRunner
     end
   end
 
-  def run_httperf
+  def run_httperf rate=nil
     @options.uri_list.each do |uri|
       authentication = get_authentication_string()
       cmd =  "#{@options.httperf} --client=0/1 --server=#{@options.server} "
       cmd << "--port=#{@options.port} --uri=\"#{uri}\" "
-      cmd << "--rate=#{@options.rate} --send-buffer=#{@options.send_buffer} "
+      cmd << "--rate=#{rate or @options.rate} "
+      cmd << "--send-buffer=#{@options.send_buffer} "
       cmd << "--recv-buffer=#{@options.recv_buffer} "
       cmd << "--add-header=\"Host:#{@options.host}\\n#{authentication}\" "
       cmd << "--num-conns=#{@options.connections} "
       cmd << "--num-call=#{@options.num_call} "
       cmd << "--hog" if @options.hog
-      puts `#{cmd}`
-      #puts cmd
+
+      res = Hash.new("")
+      IO.popen("#{cmd} 2>&1") do |pipe|
+        puts "\n#{cmd}"
+        while((line = pipe.gets))
+          res['output'] += line
+
+          case line
+          when /^Total: .*replies (\d+)/ then res['replies'] = $1
+          when /^Connection rate: (\d+\.\d)/ then res['conn/s'] = $1
+          when /^Request rate: (\d+\.\d)/ then res['req/s'] = $1
+          when /^Reply time .* response (\d+\.\d)/ then res['reply time'] = $1
+          when /^Net I\/O: (\d+\.\d)/ then res['net io (KB/s)'] = $1
+          when /^Errors: total (\d+)/ then res['errors'] = $1
+          when /^Reply rate .*min (\d+\.\d) avg (\d+\.\d) max (\d+\.\d) stddev (\d+\.\d)/ then
+            res['replies/s min'] = $1
+            res['replies/s avg'] = $2
+            res['replies/s max'] = $3
+            res['replies/s stddev'] = $4
+          when /^Reply status: 1xx=\d+ 2xx=\d+ 3xx=\d+ 4xx=\d+ 5xx=(\d+)/ then res['5xx status'] = $1
+          end
+        end
+      end
+      return res
     end
   end
 
@@ -155,6 +196,10 @@ class HttperfRunner
     @options.server      = 'localhost'            # Run on the local machine
     @options.host        = 'localhost'            # Host: header to use
     @options.rate        = 50                     # httperf rate
+    @options.low_rate    = nil                    # starting request rate, for doing multiple httperf runs
+    @options.high_rate   = nil                    # max request rate
+    @options.rate_step   = 10                     # Among to raise the rate per httperf run
+    @options.wait_time   = 120                    # Time to wait between httperf runs
     @options.port        = 80                     # By default, use http
     @options.connections = 200                    # Use 200 connections to the URL
     @options.num_call = 10
