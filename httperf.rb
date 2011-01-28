a8ec21ab7ba92f5f0f8db63a6527d00c84a0c3fb
Christopher Peplin <peplin@bueda.com>
2011-01-28 17:16:35 -0500

    Add hog and num-calls options.

--- httperf.rb
+++ httperf.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 #
 # == Synopsis
-# This script utilizes httperf to perform a series of 
+# This script utilizes httperf to perform a series of
 # calls to a list of websites, using a simple yaml config
 # for its options
 #
@@ -26,20 +26,20 @@
 # Tim Gourley (mailto:tgourley@engineyard.com)
 
 require 'rubygems'
-require 'optparse' 
-require 'rdoc/usage'
+require 'optparse'
 require 'ostruct'
 require 'yaml'
 require 'base64'
 
-VALID_OPTIONS = ['server', 'rate', 'port', 'connections', 'send_buffer', 'recv_buffer', 
-                 'uri_list', 'httperf', 'host', 'username', 'password']
+VALID_OPTIONS = ['server', 'rate', 'port', 'connections', 'send_buffer',
+  'recv_buffer', 'uri_list', 'httperf', 'host', 'username', 'password',
+  'num_call', 'hog']
 
 class OpenStruct
   def add_new(name, value)
     @table[name.to_sym] = value
     new_ostruct_member(name)
-  end  
+  end
 end
 
 class HttperfRunner
@@ -60,17 +60,17 @@ class HttperfRunner
     if parsed_options? && arguments_valid?
       # Try loading the config file
       load_yaml_options
-      
+
       # Check to make sure we have a valid httperf
       check_for_httperf
-      
+
       # Run the script
       run_httperf
     else
       output_usage
     end
   end
-  
+
   def load_yaml_options
     if @options.config
       @options.yaml = YAML::load(File.open(File.expand_path(@options.config)))
@@ -81,14 +81,18 @@ class HttperfRunner
       end
     end
   end
-  
+
   def run_httperf
     @options.uri_list.each do |uri|
       authentication = get_authentication_string()
-      cmd =  "#{@options.httperf} --client=0/1 --server=#{@options.server} --port=#{@options.port} --uri=#{uri} "
-      cmd << "--rate=#{@options.rate} --send-buffer=#{@options.send_buffer} --recv-buffer=#{@options.recv_buffer} "
+      cmd =  "#{@options.httperf} --client=0/1 --server=#{@options.server} "
+      cmd << "--port=#{@options.port} --uri=\"#{uri}\" "
+      cmd << "--rate=#{@options.rate} --send-buffer=#{@options.send_buffer} "
+      cmd << "--recv-buffer=#{@options.recv_buffer} "
       cmd << "--add-header=\"Host:#{@options.host}\\n#{authentication}\" "
-      cmd << "--num-conns=#{@options.connections} --hog | grep \"Request rate\""
+      cmd << "--num-conns=#{@options.connections} "
+      cmd << "--num-call=#{@options.num_call} "
+      cmd << "--hog" if @options.hog
       puts `#{cmd}`
       #puts cmd
     end
@@ -115,7 +119,8 @@ class HttperfRunner
   def arguments_valid?
     valid = true
 
-    # If there is a config option set and the file doesnt' exist, then we have a problem
+    # If there is a config option set and the file doesnt' exist, then we have a
+    # problem
     if (@options.config && !File::exists?(File.expand_path(@options.config)))
       puts "Invalid configuration file path #{@options.config}"
       valid = false
@@ -127,7 +132,6 @@ class HttperfRunner
   # Show the usage statement
   def output_usage
     output_version
-    RDoc::usage('usage') # gets usage from comments above
   end
 
   def output_version
@@ -135,35 +139,39 @@ class HttperfRunner
   end
 
   private
-  
+
   # Check to see if httperf is available
   def check_for_httperf
-    if !@options.httperf || @options.httperf.empty? || !File.exist?(File.expand_path(@options.httperf))
-      puts "Cannot find a valid httperf binary. Check your config and try again!"
+    if (!@options.httperf or @options.httperf.empty? or
+        !File.exist?(File.expand_path(@options.httperf)))
+      puts "Cannot find a valid httperf binary. Check your config and try again"
       exit(1)
     end
   end
 
-  # Set the default options for the script. 
-  def set_defaults    
+  # Set the default options for the script.
+  def set_defaults
     @options.config      = nil                    # By default, don't use a config
     @options.server      = 'localhost'            # Run on the local machine
     @options.host        = 'localhost'            # Host: header to use
     @options.rate        = 50                     # httperf rate
     @options.port        = 80                     # By default, use http
     @options.connections = 200                    # Use 200 connections to the URL
+    @options.num_call = 10
     @options.send_buffer = 4096                   # Send buffer
     @options.recv_buffer = 16384                  # Receive buffer
     @options.uri_list    = ['/']                  # By default, only hit the homepage
     @options.httperf     = `which httperf`.chomp  # Path to httperf (required, obviously!)
     @options.username    = nil                    # Basic auth - username
     @options.password    = nil                    # Basic auth - password
+    @options.hog         = true                   # Use as many TCP ports as neccessary
   end
-  
+
   def get_authentication_string
     authentication = ''
     if (@options.username && @options.password)
-      encoded = Base64.encode64("#{@options.username}:#{@options.password}").chomp
+      encoded = Base64.encode64(
+          "#{@options.username}:#{@options.password}").chomp
       authentication = "Authorization: Basic #{encoded}\\n"
     end
     authentication
